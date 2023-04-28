c44a96c358b4ca52f7c54aa10121926ee5b479fa
pengyyy1 <122152620+pengyyy1@users.noreply.github.com>
2023-04-28 10:20:13 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 var xBall = Math.floor((Math.random() * 1000) + 0);
 var yBall = 50;
-var xSpeed = Math.floor((Math.random() * -20) + 20);
-var ySpeed = Math.floor((Math.random() * (20)) - 20);
+var xSpeed = Math.floor((Math.random() * (-15)) + 15);
+var ySpeed = Math.floor((Math.random() * (15)) + 5);
 var score = 0
 
 function setup() {
@@ -23,12 +23,16 @@ function draw() {
   move();
   ball();
   paddle();
+
+  if (yBall >= height/1.2+height/35) {
   reset()
+  }
 
   textSize((width+height)/60);
   fill('white');
   textFont("Bungee");
   text("Score: " + score, width/30, height/12);
+
 }
 
 function ball() {
@@ -37,7 +41,7 @@ function ball() {
 }
 
 function paddle() {
-  if (((yBall/2+width/35)-(width+height)/140 >= height/1.2) && (xBall > mouseX-width/14 && xBall < mouseX+width/14)) {
+  if (((yBall+width/35)-(width+height)/140 >= height/1.2) && (xBall > mouseX-width/14 && xBall < mouseX+width/14)) {
     xSpeed *= -1;
     ySpeed *= -1;
     score = score+1;
@@ -59,21 +63,15 @@ function bounce() {
     xSpeed *= -1;
   }
 
-  if (xBall < 0) {
-    xSpeed *= -1;
-    console.log(xSpeed);
-  }
-
   if (yBall < 10) {
     ySpeed *= -1;
   }
 }
 
 function reset() {
- if (yBall >= height/1.2+height/35) {
- ySpeed = 4;
- yBall = height/8;
- score = 0;
- xBall = Math.floor(Math.random() * 300) + 50;
- }
+xBall = Math.floor((Math.random() * 1000) + 0);
+yBall = 50;
+xSpeed = Math.floor((Math.random() * (-15)) + 15);
+ySpeed = Math.floor((Math.random() * (15)) + 5);
+score = 0
 }
